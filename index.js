2fa42f67dcc2e46241a44eba6dfac690fc121d56
AFOUFA96 <87020583+AFOUFA96@users.noreply.github.com>
2022-02-09 13:31:42 +0100

    dataForm

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("my fisrt node app ");
+const express = require("express");
+const router = express.Router();
+
+const app = express();
+
+const cors = require("cors");
+const corsOptions = {
+    origin : ["http://localhost:3000"]
+};
+app.use(cors(corsOptions));
+app.use(express.json());  // pour autoriser json
+app.use(router);
+
+router.route("/")
+    .get((req,res) => {
+    // console.log("GET /");
+        res.send((req.method + req.path));
+    })
+    .post(((req,res)=>{
+        res.send((req.method + req.path));
+    })
+);
+
+router.route("/formData")
+    .get((req,res) => {
+        res.send((req.body));
+    })
+    .post((req,res) => {
+        const data = req.body;
+        console.log(data);
+        res.json(data);
+    });
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server is runnig on port ${PORT}.`);
+});
